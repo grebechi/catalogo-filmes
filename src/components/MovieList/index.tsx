a69969fdcbed2481916d60902e76ee74b7b61c56
Gabriel Rebechi <bibo.rebechi@gmail.com>
2024-12-28 03:22:36 -0300

    feat: busca paginada conforme scroll na página

--- src/components/MovieList/index.tsx
+++ src/components/MovieList/index.tsx
@@ -1,56 +1,85 @@
-'use client'
+'use client';
 
 import { useEffect, useState } from 'react';
 import './index.scss';
 import axios from 'axios';
+import debounce from 'lodash/debounce';
 import MovieCard from '../MovieCard';
 import { Movie } from '@/types/movie';
 import LoadingSpinner from '../LoadingSpinner';
 
 export default function MovieList() {
-    const [movies, setMovies] = useState<Movie[]>([]);
-    const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const [isFetchingMore, setIsFetchingMore] = useState<boolean>(false);
 
-    useEffect(() => {
-        getMovies();
-    },[])
+  useEffect(() => {
+    getMovies(currentPage);
+  }, [currentPage]);
 
-    const getMovies = async () => {
-        await axios({
-            method: 'get',
-            url: 'https://api.themoviedb.org/3/discover/movie',
-            params: {
-                api_key: process.env.NEXT_PUBLIC_TMDB_API_KEY,
-                language: 'pt-BR',
-            },
-        })
-        .then(response => {
-            setMovies(response.data.results);
-        })
-        .catch(error => {
-            console.error('Erro ao buscar filmes:', error);
-        });
-        setIsLoading(false);
-    };
+  useEffect(() => {
+    const handleScroll = debounce(() => {
+      if (
+        window.innerHeight + window.scrollY >= document.body.offsetHeight - 100 &&
+        !isFetchingMore
+      ) {
+        loadMoreMovies();
+      }
+    }, 200);
 
-    if(isLoading){
-        return(
-            <div className='loading-container'>
-                <LoadingSpinner size='80px' color='#6046ff' />
-            </div>
-        )
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, [isFetchingMore]);
+
+  const getMovies = async (page: number) => {
+    setIsLoading(true);
+    try {
+      const response = await axios({
+        method: 'get',
+        url: 'https://api.themoviedb.org/3/discover/movie',
+        params: {
+          api_key: process.env.NEXT_PUBLIC_TMDB_API_KEY,
+          language: 'pt-BR',
+          page,
+        },
+      });
+      setMovies((prevMovies) => [...prevMovies, ...response.data.results]);
+    } catch (error) {
+      console.error('Erro ao buscar filmes:', error);
     }
-    
+    setIsLoading(false);
+    setIsFetchingMore(false);
+  };
+
+  const loadMoreMovies = () => {
+    setIsFetchingMore(true);
+    setCurrentPage((prevPage) => prevPage + 1);
+  };
+
+  if (isLoading && currentPage === 1) {
     return (
-    <ul className="movie-list">
-        {movies.map((movie) =>
+      <div className="loading-container">
+        <LoadingSpinner size="80px" color="#6046ff" />
+      </div>
+    );
+  }
+
+  return (
+    <>
+      <ul className="movie-list">
+        {movies.map((movie, index) => (
             <MovieCard 
-                key={movie.id} 
+                key={`${movie.id}-${index}`} // Combina o ID do filme com o índice para gerar uma chave única
                 movie={movie}
             />
-        )}
-        
-    </ul>
-
-  )
+        ))}
+      </ul>
+      {isFetchingMore && (
+        <div className="loading-container">
+          <LoadingSpinner size="40px" color="#6046ff" />
+        </div>
+      )}
+    </>
+  );
 }
